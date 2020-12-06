27f3b0e17bd2e018457a5da9e3d9720f51c3ea68
php <panjihusnipadhila@gmail.com>
2020-12-06 09:58:33 +0700

    Signup harus email ub

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,57 +1,63 @@
-<?php 
+<?php
 
-class Login extends CI_Controller{
+class Login extends CI_Controller
+{
 
-	function __construct(){
-		parent::__construct();		
+	function __construct()
+	{
+		parent::__construct();
 		$this->load->model('m_login');
-
 	}
 
-	function index(){
+	function index()
+	{
 		$this->load->view('v_login2');
 	}
 
 
 
-	function aksi_login(){
+	function aksi_login()
+	{
 		$nim = $this->input->post('nim');
 		$Password = $this->input->post('Password');
 		$where = array(
 			'NIM' => $nim,
 			'Password' => $Password
-			);
-		$cek = $this->m_login->cek_login("mahasiswa",$where)->num_rows();
-		if($cek > 0){
+		);
+		$cek = $this->m_login->cek_login("mahasiswa", $where)->num_rows();
+		if ($cek > 0) {
 
 			$data_session = array(
 				'NIM' => $nim,
 				'status' => "login"
-				);
+			);
 
 			$this->session->set_userdata($data_session);
-			
-			redirect(base_url("main"));
 
-		}else{
+			redirect(base_url("main"));
+		} else {
 			echo "Username dan password salah !";
 		}
 	}
 
-	function logout(){
+	function logout()
+	{
 		$this->session->sess_destroy();
 		redirect(base_url('login'));
 	}
 
-	function signup(){
+	function signup()
+	{
 		$this->load->helper('url');
 		$this->load->model('m_login');
 		$Nama = $this->input->post('nama');
 		$Password = $this->input->post('password');
 		$email = $this->input->post('email');
 		$NIM = $this->input->post('nim');
-		$this->m_login->signup($Nama, $NIM, $email, $Password);
-		echo "sukses";
-		redirect(base_url('login'));
+		if (preg_match("/@student.ub.ac.id/", $email)) {
+			$this->m_login->signup($Nama, $NIM, $email, $Password);
+			echo "sukses";
+			redirect(base_url('login'));
+		}
 	}
 }
