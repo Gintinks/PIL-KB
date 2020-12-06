1c31219964c00810a7c19683084dd2ce8da6556d
php <panjihusnipadhila@gmail.com>
2020-12-07 01:36:44 +0700

    Model dan Controller Admin fix

--- application/controllers/mainAdmin.php
+++ application/controllers/mainAdmin.php
@@ -13,7 +13,7 @@ class MainAdmin extends CI_Controller{
 	function index(){
 		$this->load->helper('url');
 		$this->load->model('m_admin');
-		$posts = $this->m_admin->get_data_mahasiswa($this->session->userdata('username'));
+		$posts = $this->m_admin->get_data_admin($this->session->userdata('username'));
 		$data['posts'] = $posts;
 		$this->load->view('home_admin', $data);
 
