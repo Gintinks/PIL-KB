b6ae48c1a01ec12fd6b6885e0791e186fee40a3e
Your Name <you@example.com>
2020-12-11 18:12:35 +0800

    vg

--- application/views/home_pemilihan.php
+++ application/views/home_pemilihan.php
@@ -1,244 +1,252 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Country Road, Take me Home</title>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Country Road, Take me Home</title>
 
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 	<style>
-
-.titlecard {
-  color: grey;
-  font-size: 18px;
-}
-
-
-
+		.titlecard {
+			color: grey;
+			font-size: 18px;
+		}
 	</style>
-  </head>
-  <body>
+</head>
 
-    <div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<nav class="navbar navbar-expand-lg navbar-light bg-light static-top navbar-dark bg-dark">
-				 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="navbar-toggler-icon"></span>
-				</button> <a class="navbar-brand" href="#">PIL-KB</a>
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="navbar-nav">
-						<li class="nav-item active">
-							 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-						</li>
-						<li class="nav-item">
-							 <a class="nav-link" href="#">Pilih KB</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Kritik dan Saran</a>
-					   </li>
-						
-					</ul>
-					
-					<!-- <ul class="navbar-nav ml-md-auto">
-						<li class="nav-item active">
-							 <a class="nav-link" href="#">Link <span class="sr-only">(current)</span></a>
-						</li>
-						<li class="nav-item dropdown">
-							 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
-							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-								 <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
-								<div class="dropdown-divider">
-								</div> <a class="dropdown-item" href="#">Separated link</a>
+<body>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<nav class="navbar navbar-expand-lg navbar-light bg-light static-top navbar-dark bg-dark">
+
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="navbar-toggler-icon"></span>
+					</button> <a class="navbar-brand" href="#">PIL-KB</a>
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="navbar-nav">
+							<li class="nav-item active">
+								<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Pilih KB</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Kritik dan Saran</a>
+							</li>
+
+						</ul>
+
+
+					</div>
+				</nav>
+				<div class="jumbotron card card-block">
+					<div class="conatiner" style="padding:50px;" id="aboutTab">
+
+						<div class="row">
+
+							<div class="col-sm-4 text-center">
+
+								<img src="assets2\images\icons\Nominee.png" width="100" height="100" alt="" />
+								<h2 class="normalFont" style="font-size:28px;">Nominasi</h2>
+								<p><em>Pengguna Dapat Melihat Kandidat Dari Ketua BEM</em></p>
+
 							</div>
-						</li>
-					</ul> -->
+
+							<div class="col-sm-4 text-center">
+
+								<img src="assets2\images\icons\Vote.png" width="100" height="100" alt="" />
+								<h2 class="normalFont" style="font-size:28px;">Voting Online</h2>
+								<p><em>Melakukan Pemilihan Langsung Secara Online Tanpa Repot</em></p>
+
+							</div>
+							
+							<div class="col-sm-4 text-center">
+
+								<img src="assets2\images\icons\Stats.png" width="100" height="100" alt="" />
+								<h2 class="normalFont" style="font-size:28px;">Lihat Hasil</h2>
+								<p><em>Hasil Dapat Langsung Dilihat Sesaat Setelah Pemilihan Selesai</em></p>
+
+							</div>
+
+
+
+						</div>
+
+
+					</div>
 				</div>
-			</nav>
-			<div class="jumbotron card card-block">
-				<h2>
-					Hello, world!
-				</h2>
-				<p>
-					This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.
-				</p>
-				<p>
-					<a class="btn btn-primary btn-large" href="#">Learn more</a>
-				</p>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="card" 
-		style ="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+				<div class="row">
+					<div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+					max-width: 300px;
+		margin: auto;
+		text-align: center;">
+						<img src="2b Shrug.jpg" alt="John" style="width:100%">
+						<h1>John Doe</h1>
+						<p class="titlecard">
+							CEO & Founder, Example</p>
+						<p class="titlecard">
+							TTL</p>
+						<p class="titlecard">
+							Jenis klamin</p>
+						<!-- <p>Harvard University</p>		
+						<a href="#"><i class="fa fa-dribbble"></i></a>
+		<a href="#"><i class="fa fa-twitter"></i></a>
+	<a href="#"><i class="fa fa-linkedin"></i></a
+					<a href="#"><i class="fa fa-facebook"></i></a>
+	<p><button>Contact</button></p> -->
+						<div id="accordion">
+							<div class="card">
+								<div class="card-header" id="headingOne">
+									<h5 class="mb-0">
+										<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+											Ntar deskripsi, mimpi, dan harapan disini
+										</button>
+									</h5>
+								</div>
+
+								<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+									<div class="card-body">
+										ntar ini isinya deskripsi database
+									</div>
+								</div>
+							</div>
+
+							<div class="card">
+								<div class="card-header" id="headingOne">
+									<h5 class="mb-0">
+										<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+											Trus yang ini prestasi
+										</button>
+									</h5>
+								</div>
+
+								<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+									<div class="card-body">
+										ntar ini isinya prestasi
+									</div>
+								</div>
+							</div>
+
+						</div>
+					</div>
+					<div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 		max-width: 300px;
 		margin: auto;
 		text-align: center;">
-			<img src="2b Shrug.jpg" alt="John" style="width:100%">
-			<h1>John Doe</h1>
-			<p class="titlecard">
-			CEO & Founder, Example</p>
-			<p class="titlecard" >
-			TTL</p>
-			<p class="titlecard" >
-			Jenis klamin</p>
-			<!-- <p>Harvard University</p>
+						<img src="Mylife.png" alt="John" style="width:100%">
+						<h1>Lyle Walt</h1>
+						<p class="titlecard">
+							CEO & Founder, Example</p>
+						<p class="titlecard">
+							TTL</p>
+						<p class="titlecard">
+							Jenis klamin</p>
+						<!-- <p>Harvard University</p>
 			<a href="#"><i class="fa fa-dribbble"></i></a>
 			<a href="#"><i class="fa fa-twitter"></i></a>
 			<a href="#"><i class="fa fa-linkedin"></i></a>
 			<a href="#"><i class="fa fa-facebook"></i></a>
 			<p><button>Contact</button></p> -->
-			<div id="accordion">
-				<div class="card">
-				  <div class="card-header" id="headingOne">
-					<h5 class="mb-0">
-					  <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-						Ntar deskripsi, mimpi, dan harapan disini
-					  </button>
-					</h5>
-				  </div>
-			  
-				  <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-					<div class="card-body">
-						ntar ini isinya deskripsi database
-					</div>
-				  </div>
-				</div>
+						<div id="accordion">
+							<div class="card">
+								<div class="card-header" id="headingOne">
+									<h5 class="mb-0">
+										<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+											Ntar deskripsi, mimpi, dan harapan disini
+										</button>
+									</h5>
+								</div>
 
-				<div class="card">
-					<div class="card-header" id="headingOne">
-					  <h5 class="mb-0">
-						<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-						  Trus yang ini prestasi
-						</button>
-					  </h5>
+								<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+									<div class="card-body">
+										ntar ini isinya deskripsi database
+									</div>
+								</div>
+							</div>
+
+							<div class="card">
+								<div class="card-header" id="headingOne">
+									<h5 class="mb-0">
+										<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+											Trus yang ini prestasi
+										</button>
+									</h5>
+								</div>
+
+								<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+									<div class="card-body">
+										ntar ini isinya prestasi
+									</div>
+								</div>
+							</div>
+
+						</div>
 					</div>
-				
-					<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-					  <div class="card-body">
-						  ntar ini isinya prestasi
-					  </div>
-					</div>
-				</div>
-				
-			</div>
-		</div>
-		<div class="card" 
-		style ="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+					<div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 		max-width: 300px;
 		margin: auto;
 		text-align: center;">
-			<img src="Mylife.png" alt="John" style="width:100%">
-			<h1>Lyle Walt</h1>
-			<p class="titlecard">
-			CEO & Founder, Example</p>
-			<p class="titlecard" >
-			TTL</p>
-			<p class="titlecard" >
-			Jenis klamin</p>
-			<!-- <p>Harvard University</p>
+						<img src="happywolf.png" alt="John" style="width:100%">
+						<h1>Lawrence</h1>
+						<p class="titlecard">
+							CEO & Founder, Example</p>
+						<p class="titlecard">
+							TTL</p>
+						<p class="titlecard">
+							Jenis klamin</p>
+						<!-- <p>Harvard University</p>
 			<a href="#"><i class="fa fa-dribbble"></i></a>
 			<a href="#"><i class="fa fa-twitter"></i></a>
 			<a href="#"><i class="fa fa-linkedin"></i></a>
 			<a href="#"><i class="fa fa-facebook"></i></a>
 			<p><button>Contact</button></p> -->
-			<div id="accordion">
-				<div class="card">
-				  <div class="card-header" id="headingOne">
-					<h5 class="mb-0">
-					  <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-						Ntar deskripsi, mimpi, dan harapan disini
-					  </button>
-					</h5>
-				  </div>
-			  
-				  <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-					<div class="card-body">
-						ntar ini isinya deskripsi database
+						<div id="accordion">
+							<div class="card">
+								<div class="card-header" id="headingOne">
+									<h5 class="mb-0">
+										<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+											Ntar deskripsi, mimpi, dan harapan disini
+										</button>
+									</h5>
+								</div>
+
+								<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+									<div class="card-body">
+										ntar ini isinya deskripsi database
+									</div>
+								</div>
+							</div>
+
+							<div class="card">
+								<div class="card-header" id="headingOne">
+									<h5 class="mb-0">
+										<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+											Trus yang ini prestasi
+										</button>
+									</h5>
+								</div>
+
+								<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+									<div class="card-body">
+										ntar ini isinya prestasi
+									</div>
+								</div>
+							</div>
+
+						</div>
 					</div>
-				  </div>
 				</div>
 
-				<div class="card">
-					<div class="card-header" id="headingOne">
-					  <h5 class="mb-0">
-						<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-						  Trus yang ini prestasi
-						</button>
-					  </h5>
-					</div>
-				
-					<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-					  <div class="card-body">
-						  ntar ini isinya prestasi
-					  </div>
-					</div>
-				</div>
-				
-			</div>
-		</div><div class="card" 
-		style ="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-		max-width: 300px;
-		margin: auto;
-		text-align: center;">
-			<img src="happywolf.png" alt="John" style="width:100%">
-			<h1>Lawrence</h1>
-			<p class="titlecard">
-			CEO & Founder, Example</p>
-			<p class="titlecard" >
-			TTL</p>
-			<p class="titlecard" >
-			Jenis klamin</p>
-			<!-- <p>Harvard University</p>
-			<a href="#"><i class="fa fa-dribbble"></i></a>
-			<a href="#"><i class="fa fa-twitter"></i></a>
-			<a href="#"><i class="fa fa-linkedin"></i></a>
-			<a href="#"><i class="fa fa-facebook"></i></a>
-			<p><button>Contact</button></p> -->
-			<div id="accordion">
-				<div class="card">
-				  <div class="card-header" id="headingOne">
-					<h5 class="mb-0">
-					  <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-						Ntar deskripsi, mimpi, dan harapan disini
-					  </button>
-					</h5>
-				  </div>
-			  
-				  <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-					<div class="card-body">
-						ntar ini isinya deskripsi database
-					</div>
-				  </div>
-				</div>
 
-				<div class="card">
-					<div class="card-header" id="headingOne">
-					  <h5 class="mb-0">
-						<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-						  Trus yang ini prestasi
-						</button>
-					  </h5>
-					</div>
-				
-					<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-					  <div class="card-body">
-						  ntar ini isinya prestasi
-					  </div>
-					</div>
-				</div>
-				
-			</div>
-		</div>
-	</div>
-</div>
+				<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+				<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+				<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</body>
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-  </body>
 </html>
