9fa363152c26c4d8b325254883bb08ea0bf82e62
Your Name <you@example.com>
2020-12-20 02:53:23 +0800

    idk lul

--- application/views/pilih_kb.php
+++ application/views/pilih_kb.php
@@ -53,14 +53,14 @@
         <div class="row" style="border: 5px solid #999;margin-left: 10px; ;margin-right: 10px; background-color: #4DE1FF;">
             <div class="col-sm-12">
                 <div class="page-header" style="padding-bottom:30px;">
-                    <form action="createUser.php" method="post">
+                    <form action="<?php echo base_url("main/vote"); ?>" method="post">
                         <br>
                         <h1 class="specialHead" style="color:black">Pilih Calon Ketua Yang Ingin Anda Pilih</h1>
                         <div class="col-sm-12" style="border: 1px solid;">
                             <?php $i = 1 ?>
                             <?php foreach ($posts2 as $post) : ?>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios<?php echo $i; ?>" value="option<?php echo $i; ?>" checked style="transform: scale(1.5);margin-top: 25px;">
+                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios<?php echo $i; ?>" value="<?php echo $post->ID_ketua; ?>" checked style="transform: scale(1.5);margin-top: 25px;">
                                     <label class="form-check-label" for="exampleRadios<?php echo $i; ?>" style="font-size: 40px">
                                         <?php echo $post->nama; ?>
                                     </label>
@@ -72,9 +72,18 @@
                             ?>
                         </div>
                         <br>
-                        <a href="#" class="btn btn-info"> <strong>Pilih Ketua</strong></a>
+                        <a href="main" class="btn btn-info"type="submit" name="submit"> <strong>Pilih Ketua</strong></a>
                     </form>
                 </div>
+                <?php
+                if (isset($_POST['submit'])) {
+                    if (!empty($_POST['exampleRadios'])) {
+                        echo '  ' . $_POST['exampleRadios'];
+                    } else {
+                        echo 'Please select the value.';
+                    }
+                }
+                ?>
 
             </div>
 
