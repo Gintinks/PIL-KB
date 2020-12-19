03a3dcfc6b6b00dcd7a0036fa31945ebd327ef75
Your Name <you@example.com>
2020-12-20 01:18:39 +0800

    idk

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -13,16 +13,20 @@ class Main extends CI_Controller{
 	function index(){
 		$this->load->helper('url');
 		$this->load->model('m_mahasiswa');
+		$this->load->model('m_calon');
 		$posts = $this->m_mahasiswa->get_data_mahasiswa($this->session->userdata('NIM'));
 		$posts2 = $this->m_calon->get_data();
-		$posts3 = $this->m_pemilihan->get_data_pemilihan(2020);
+	//	$posts3 = $this->m_pemilihan->get_data_pemilihan(2020);
+	
+		
 		$data['posts'] = $posts;
 		$data['posts2'] = $posts2;
-		$data['posts3'] = $posts3;
-		if (date_diff($posts3['end_time'], getDate()) > 0 ){
-			$this->load->view('');
-		} else {
-			$this->load->view('home_pemilihan');
-		}
+	//	$data['posts3'] = $posts3;
+	
+		//  if (date_diff($posts3['end_time'], getDate()) > 0 ){
+		//  	$this->load->view('');
+		//  } else {
+		 	$this->load->view('home_pemilihan');
+		// }
 	}
 }
